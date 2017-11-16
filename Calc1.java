d969de1a3c8e76c943b1b21513bd40a5327f16ec
onkar.kamatkar <onkar.kamatkar@meganexus.com>
2017-11-16 17:31:55 +0530

    added add, avg calc..

--- Calc1.java
+++ Calc1.java
@@ -1,5 +1,14 @@
 public class Calc1 {
 	public static void main(String[] args) {
 		System.out.println("Calc1 operations..");
+		
+		int x = 10, y = 20;
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("Addition = " + add(x, y));
+	}
+	
+	private static int add(int a, int b) {
+		return a + b;
 	}
 }
