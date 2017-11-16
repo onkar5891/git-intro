1642dddf25932d08934d25658bf17166bfccf0dc
onkar.kamatkar <onkar.kamatkar@meganexus.com>
2017-11-16 17:35:45 +0530

    added sub calc..

--- Calc1.java
+++ Calc1.java
@@ -1,5 +1,14 @@
 public class Calc1 {
 	public static void main(String[] args) {
-		System.out.println("Calc1 operations..");
+		System.out.println("Calc2 operations..");
+		
+		int x = 10, y = 20;
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("Subtraction = " + sub(x, y));
 	}
+	
+	private static int sub(int a, int b) {
+		return a – b;
+}
 }
